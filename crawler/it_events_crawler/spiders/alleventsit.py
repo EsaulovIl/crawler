26cf03252bbf0606db0cdb91e6761592213cec72
EsaulovIl <ilya.esaulov2015@yandex.ru>
2025-06-10 23:14:40 +0300

    update commit

---
+++ crawler/it_events_crawler/spiders/alleventsit.py
@@ -0,0 +1,79 @@
+import scrapy
+import re
+from w3lib.html import remove_tags
+import html
+from it_events_crawler.items import EventItem
+
+
+class AllEventsIT(scrapy.Spider):
+    name = "alleventsit"
+    start_urls = [
+        "https://all-events.ru/events/calendar/city-is-nizhniy_novgorod/"
+    ]
+
+    def get_title(self, info):
+        return info.css("div.event_flex_content").css("div.event_order_1").css("a.event_name_new::text").get()
+
+    def get_start_date(self, info):
+        result = info.xpath('.//div[@class="event-date"]/div[not(@class)]/text()').get()
+        return result[0:10]
+
+    def get_end_date(self, info):
+        result = info.xpath('.//div[@class="event-date"]/div[not(@class)]/text()').get()
+        result = result.replace('\xa0', ' ').strip()
+        m = re.search(r'(\d{2}\.\d{2}\.\d{4})(?:\s*-\s*(\d{2}\.\d{2}\.\d{4}))?', result)
+        return m.group(2) or m.group(1) if m else None
+
+    def get_location(self, info):
+        sel = info.css(
+            "div.event_flex_content div.event_width.order_3 div.event_info_new "
+            "a.event_info_new_text.svg_hybrid span:nth-of-type(2)::text, "
+            "div.event_flex_content div.event_width.order_3 div.event_info_new "
+            "a.event_info_new_text.svg_ span:nth-of-type(2)::text, "
+            "div.event_flex_content div.event_width.order_3 div.event_info_new "
+            "a.event_info_new_text.svg_offline span:nth-of-type(2)::text"
+        )
+        return ', '.join(sel.getall()).strip()
+
+    def get_tags(self, info):
+        return info.css("div.teg_content a::text").getall()
+
+    def parse(self, response):
+        base = "https://all-events.ru"
+        for info in response.css("div.event_flex_item"):
+            item = EventItem(
+                title=self.get_title(info),
+                start_date=self.get_start_date(info),
+                end_date=self.get_end_date(info),
+                location=self.get_location(info),
+                url=response.url,
+                organizer="Не указано",
+                event_type="Не указано",
+                event_format="Не указано"
+            )
+            rel_link = info.css("div.btn_events a::attr(href)").get()
+            if rel_link:
+                yield response.follow(
+                    base + rel_link,
+                    callback=self.parse_event,
+                    meta={"item": item},
+                    dont_filter=True
+                )
+            else:
+                item["description"] = "Не указано"
+                yield item
+
+    def clear_description(self, raw_html):
+        text = remove_tags(raw_html)
+        text = html.unescape(text)
+        text = text.replace('\r', '').replace('\t', '')
+        lines = [re.sub(r' {2,}', ' ', line).strip() for line in text.split('\n')]
+        lines = [line for line in lines if line]
+
+        return '\n'.join(lines)
+
+    def parse_event(self, response):
+        item = response.meta["item"]
+        raw = response.css("div.events-content").get()
+        item["description"] = self.clear_description(raw) if raw else "Не указано"
+        yield item
---
+++ crawler/it_events_crawler/spiders/itc2go.py
@@ -0,0 +1,104 @@
+import scrapy
+import re
+from w3lib.html import remove_tags
+from urllib.parse import urljoin
+import html
+from it_events_crawler.items import EventItem
+
+
+class Itc2Go(scrapy.Spider):
+    name = "itc2go"
+    start_urls = [
+        "https://ict2go.ru/regions/Nizhny_Novgorod/"
+    ]
+
+    def get_title(self, info):
+        return info.css("a.media-left.image-link").attrib["title"]
+
+    def get_start_date(self, info):
+        result = info.css("div.date-place::text").get()
+        match = re.search(r'(\d{2}\.\d{2}\.\d{4})(?:\s*\|\s*(\S.*))?', result)
+        if match:
+            date = match.group(1)
+            city = match.group(2).strip() if match.group(2) else None
+        return date
+
+    def get_end_date(self, info):
+        return None
+
+    def get_location(self, info):  # надо переходить по ссылке
+        return ""
+
+    def get_description(self, info):
+        return ""
+
+    def get_tags(self, info):
+        tags = info.css("div.event-themes a::text").getall()
+        return tags
+
+    def parse(self, response):
+        base = "https://ict2go.ru"
+        for info in response.css("div.index-events-item.media"):
+            rel_link = info.css("a.event-title").attrib["href"]
+            if "/events" not in rel_link:
+                continue
+
+            item = EventItem(
+                title=self.get_title(info),
+                start_date=self.get_start_date(info),
+                # url=response.url,
+                organizer="Не указано",
+                event_type="Не указано",
+                event_format="Не указано"
+            )
+
+            href = info.css("a.event-title::attr(href)").get()
+            if href and "/events/" in href:
+                abs_url = urljoin(response.url, href)
+                yield response.follow(
+                    abs_url,
+                    callback=self.parse_event,
+                    meta={"item": item},
+                    dont_filter=True
+                )
+                item["url"] = abs_url
+            else:
+                item["description"] = "Не указано"
+                yield item
+
+    def clear_description(self, raw_html):
+        text = remove_tags(raw_html)
+        text = html.unescape(text)
+        text = text.replace('\r', '').replace('\t', '')
+        lines = [re.sub(r' {2,}', ' ', line).strip() for line in text.split('\n')]
+        lines = [line for line in lines if line]
+
+        return '\n'.join(lines)
+
+    def parse_event(self, response):
+        item = response.meta["item"]
+
+        date_info_html = response.css("p.date-info").get()
+        if date_info_html:
+            date_info_text = self.clear_description(date_info_html)
+
+            date_range = re.search(r'(\d{2}\.\d{2}\.\d{4})\s*-\s*(\d{2}\.\d{2}\.\d{4})', date_info_text)
+            single_date = re.search(r'(\d{2}\.\d{2}\.\d{4})', date_info_text)
+
+            if date_range:
+                item["end_date"] = date_range.group(2)
+            elif single_date:
+                item["end_date"] = single_date.group(1)
+            else:
+                item["end_date"] = "Не указано"
+        else:
+            item["end_date"] = "Не указано"
+
+        raw = response.css("div.tab-item.description-info").get()
+        item["description"] = self.clear_description(raw) if raw else "Не указано"
+
+        location = response.css("p.place-info").get()
+        item["location"] = self.clear_description(location)[len("Место проведения: "):].replace("\n",
+                                                                                                "") if location else "Не указано"
+
+        yield item
